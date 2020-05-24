7afd5d69710e99593ea59de9b588199a9e2dc703
shohei morita <shomorita1985@gmail.com>
2020-05-24 12:19:48 +0900

    Task管理の基本的なCRUD機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,60 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show edit update destroy)
+
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
-  def show
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    elsif @task.save
+      redirect_to tasks_path
+      flash[:notice] = "タスクの登録が完了しました。"
+    else
+      render :new
+    end
   end
 
-  def edit
+  def show; end
+
+  def edit; end
+
+  def update
+    if params[:back]
+      redirect_to tasks_path
+    elsif @task.update(task_params)
+      redirect_to tasks_path
+      flash[:notice] = "タスクの編集が完了しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+    flash[:danger] = "タスクを削除しました"
+  end
+
+  def confirm
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:theme, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,9 @@
 module TasksHelper
+  def choose_new_or_edit
+    if action_name == "new" || action_name == "create" || action_name === "confirm"
+      confirm_tasks_path
+    else action_name == "edit"
+      task_path
+    end
+  end
 end

49d2a6e75b3941a68f7fdc84ef37a0f47bcdfb42
shohei morita <shomorita1985@gmail.com>
2020-05-26 22:23:11 +0900

    バリデーションテスト設定

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'タスク管理機能', type: :model do
+  it 'titleが空ならバリデーションが通らない' do
+    task = Task.new(theme: '', content: '失敗テスト')
+    expect(task).not_to be_valid
+  end
+  it 'contentが空ならバリデーションが通らない' do
+    task = Task.new(theme: '失敗テスト', content: '')
+    expect(task).not_to be_valid
+  end
+  it 'titleとcontetnに内容が記載されていればバリデーションが通る' do
+    task = Task.new(theme: '通過テスト', content: '通過テスト')
+    expect(task).to be_valid
+  end
+
 end

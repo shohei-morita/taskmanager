6afa502d4e6668aa403957dc9ed05ddafd253bfb
shohei morita <shomorita1985@gmail.com>
2020-06-06 15:27:03 +0900

    コード修正2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end

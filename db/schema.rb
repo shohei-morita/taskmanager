acff0b1cb6b57ed3571459913612e44fdc5d749e
shohei morita <shomorita1985@gmail.com>
2020-06-07 11:39:07 +0900

    Fix
    labelモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_132615) do
+ActiveRecord::Schema.define(version: 2020_06_07_022953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "label"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_labels_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "theme", null: false
@@ -34,9 +42,10 @@ ActiveRecord::Schema.define(version: 2020_06_04_132615) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
+    t.boolean "admin", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "labels", "tasks"
   add_foreign_key "tasks", "users"
 end

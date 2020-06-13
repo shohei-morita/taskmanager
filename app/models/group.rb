d05db3942a9fa08cf09bec338e9237210333623c
shohei morita <shomorita1985@gmail.com>
2020-06-13 10:02:58 +0900

    Fix
    UserモデルとGroupモデルの多対多アソシエーションの設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  validates :group_name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :labels, dependent: :destroy
+  has_many :groups, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },

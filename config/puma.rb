3afef2b92311099f04631114a328ca7f422f4967
shohei morita <shomorita1985@gmail.com>
2020-06-21 23:24:47 +0900

    微修正

--- config/puma.rb
+++ config/puma.rb
@@ -34,8 +34,3 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
-
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
